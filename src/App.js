b1883d4a684133d6827be1d5a6a89482e8304962
Rakibul583 <islammdrakibul583@gmail.com>
2024-07-11 18:43:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const products = [
+    { name: 'T-shirt', price: '450' },
+    { name: 'Shirt', price: '800' },
+    { name: 'Pant', price: '1000' },
+    { name: 'Panjabi', price: '1200' },
+    { name: 'Saree', price: '2000'}
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       {
+         products.map(product =>< Product product = {product}></Product>)
+       }
       </header>
     </div>
   );
 }
+
+
+//1st component
+function Product(props) {
+  const productStyle = {
+    border: '5px solid gray',
+    margin: '2px',
+    borderRadius: '10px',
+    backgroundColor: 'lightgray',
+    height: '300px',
+    width: '250px',
+    float: 'left',
+  }
+  //const { name, price } = props.product;
+ // console.log(name, price);
+  return (
+    <div style={productStyle}>
+      <h3>{props.product.name}</h3>
+      <h5>{props.product.price}</h5>
+      <button>Buy now</button>
+    </div>
+  )
+}
+
 
 export default App;
